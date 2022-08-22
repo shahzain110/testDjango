821a821ba0c551c57bd0a10bb5ebcc27c333c48a
shahzain110 <shahzainhaidernaqvi@gmail.com>
2022-08-22 15:59:38 +0500

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wpkey28tanvra7&zm+87@@l@sihd(g(_*5m)t-!&$f7hg+*0%6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['test-django2.herokuapp.com']
 
